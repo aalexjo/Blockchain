71cbcb3121ecfbb806900ce023c78d13b3aa14b8
aalexjo <alexanjohansen@gmail.com>
2018-11-05 16:46:43 +0100

    started work on reliable broadcast

--- da18_project_template/reliableBroadcast.hpp
+++ da18_project_template/reliableBroadcast.hpp
@@ -1,20 +1,36 @@
 #include "perfectLink.hpp"
 #include <vector>
+#include <string>
+#include <map>
+
+typedef void(*RBMessageCallback)(const char *ip, char *data, int datalength);
+
+
+typedef struct {
+	int port;
+	RBMessageCallback callback;
+} RBThreadList;
 
 class reliableBroadcast{
 public:
-  reliableBroadcast(int m);
+  reliableBroadcast(int n);
 
-  void broadcast(char const * data, int dataLength);
+  void broadcast(string data);
   void receiver();
   bool canDeliver();
+
+
 private:
-  perfectLink link;
-  int m;
-  std::vector<bool> delivered;
-  std::vector<bool> forward;
-  std::vector<bool> correct;
-  std::vector<std::vector<bool>> ack;
+	void pp2pCallback(const char * ip, char * data, int datalength);
+	int n; //num of Procsess
+	perfectLink link;
+	unsigned int m;
+	std::vector<std::string> delivery;
+	std::map<char,bool> delivered;
+	std::vector<bool> forward;
+	std::vector<bool> correct;
+	std::map<char, int> ack;
 
 
 }
+
