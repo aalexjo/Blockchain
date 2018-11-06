91d7993be7a3a92e52aef8f4f677842cd1e08c1d
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2018-11-07 00:07:43 +0100

    Test has now more processes

--- da18_project/test_correctness_small.sh
+++ da18_project/test_correctness_small.sh
@@ -8,9 +8,9 @@
 
 # time to wait for correct processes to broadcast all messages (in seconds)
 # (should be adapted to the number of messages to send)
-time_to_finish=2
+time_to_finish=4
 
-init_time=2
+init_time=4
 
 # configure lossy network simulation
 sudo tc qdisc change dev lo root netem delay 50ms 200ms loss 10% 25% reorder 25% 50%
@@ -18,12 +18,13 @@ sudo tc qdisc change dev lo root netem delay 50ms 200ms loss 10% 25% reorder 25%
 # compile (should output: da_proc)
 make
 
-echo "2
-0 127.0.0.1 11001
-1 127.0.0.1 11002" > membership
+echo "3
+1 127.0.0.1 11001
+2 127.0.0.1 11002
+3 127.0.0.1 11003" > membership
 
-# start 2 processes, each broadcasting 100 messages
-for i in `seq 1 2`
+# start 3 processes, each broadcasting 100 messages
+for i in `seq 1 3`
 do
     ./da_proc $i membership 3 &
     da_proc_id[$i]=$!
@@ -33,7 +34,7 @@ done
 sleep $init_time
 
 # start broadcasting
-for i in `seq 1 2`
+for i in `seq 1 3`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	      kill -USR1 "${da_proc_id[$i]}"
@@ -44,7 +45,7 @@ done
 sleep $time_to_finish
 
 # stop all processes
-for i in `seq 1 2`
+for i in `seq 1 3`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	kill -TERM "${da_proc_id[$i]}"
@@ -52,7 +53,7 @@ do
 done
 
 # wait until all processes stop
-for i in `seq 1 2`
+for i in `seq 1 3`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	    wait "${da_proc_id[$i]}"
