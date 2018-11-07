aa8a65bbae9657fb93c20ccf299bc793be17ea74
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2018-11-07 20:07:37 +0100

    Updated with network fix

--- da18_project/test_correctness.sh
+++ da18_project/test_correctness.sh
@@ -13,6 +13,7 @@ time_to_finish=5
 init_time=5
 
 # configure lossy network simulation
+sudo tc qdisc add dev lo root netem 2>/dev/null
 sudo tc qdisc change dev lo root netem delay 50ms 200ms loss 10% 25% reorder 25% 50%
 
 # compile (should output: da_proc)
