1cd3f2bca95a3e91d2490eb807e62e8fd7767eb5
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2018-12-16 15:18:45 +0100

    updated for refactor

--- da18_project/da_proc.cpp
+++ da18_project/da_proc.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sstream>
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
@@ -10,7 +11,8 @@
 #include <string>
 #include <fstream>
 #include <thread>
-#include "client.hpp"
+
+#include "p2pp.hpp"
 
 using namespace std;
 
@@ -44,35 +46,50 @@ int main(int argc, char** argv) {
 
 	//parse arguments, including membership
   //assert(argc == 4);
-  int process_i = atoi(argv[1]) - 1;
+  int pid = atoi(argv[1]) - 1;
   char* file_name = argv[2];
-  int message_n = atoi(argv[3]);
+  int messageNbr= atoi(argv[3]);
 
   //parse membership
   fstream  membership;
   int id, port;
   string ip;
 
-  int process_n;
-  vector <int> ids;
-  vector <string> ips;
-  vector <int> ports;
+  int processNbr;
+  vector <process_s> processes;
   membership.open(file_name, ios::in);
-  membership >> process_n;
-  while(membership >> id >> ip >> port) {
-    ids.push_back(id);
-    ips.push_back(ip);
-    ports.push_back(port);
+  membership >> processNbr;
+  vector <bool> dependencies(processNbr, false);
+  // Get id:ip:port
+  for(int i = 0; i < processNbr; i++) {
+    membership >> id >> ip >> port;
+    process_s process = { id, ip, port };
+    processes.push_back(process);
   }
-  membership.close();
+
+  // Get dependencies
+  string line;
+  for(int i = 0; i < processNbr; i++) {
+    getline(membership, line); // Get line till matches current pid
+    if(i == pid) {
+      stringstream pidLine(line);
+      pidLine.ignore();
+      int dep;
+      while(pidLine >> dep) {
+        dependencies[dep-1] = true;
+      }
+    }
+  }
+	membership.close();
 
 	//initialize application
-  Client client(process_i, process_n, message_n, ids, ips, ports);
+  P2PP p2pp(pid, processNbr, messageNbr, processes, nullptr);
+  exit(0)
   //client.display();
 
 	//start listening for incoming UDP packets
 	printf("Initializing.\n");
-  thread receiveMsgs(&Client::startReceiving, &client);
+  thread receiveMsgs(&UDP::startReceiving, &p2pp);
 
 	//wait until start signal
 	while(wait_for_start) {
@@ -84,7 +101,15 @@ int main(int argc, char** argv) {
 
 	//broadcast messages
 	printf("Broadcasting messages.\n");
-  client.broadcastMessages();
+  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sockfd == -1){
+    perror("cannot open socket");
+    exit(1);
+  }
+  msg_s msg = { pid, pid, 0, nullptr, false };
+  for(int dst = 0; dst < processNbr; dst++) {
+    p2pp.send(msg, dst, sockfd);
+  }
 
 	//wait until stopped
 	while(1) {
