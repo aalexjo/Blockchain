8e1920eca06ceca487d877bd542aa7ae27030294
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2018-12-16 15:14:32 +0100

    Fixed callback syntax with std::bind

--- da18_project/p2pp.hpp
+++ da18_project/p2pp.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <functional>
 
 #include "udp.hpp"
 
@@ -14,18 +15,19 @@ private :
   function<void(msg_s*)> triggerCallbackP2PP;
 
 public :
-  P2PP::P2PP(int pid, int processNbr, int messageNbr, vector<process_s> processes, function<void(msg_s*)> callback) :
-    UDP(pid, processNbr, messageNbr, processes, &P2PP::pp2pReceive);
-
   void send(msg_s msg, int dst, int sockfd) {
     UDP::send(msg, dst, sockfd);
   }
 
   void pp2pReceive(msg_s* msg) {
-    if(!deliveredPL[msg.creator][msg.seq_nbr][msg.src]) {
-      deliveredPL[msg.creator][msg.seq_nbr][msg.src] = true;
-      triggerCallbackP2PP(msg)
+    if(!deliveredPL[msg->creator][msg->seq_nbr][msg->src]) {
+      deliveredPL[msg->creator][msg->seq_nbr][msg->src] = true;
+      triggerCallbackP2PP(msg);
     }
   }
+
+  P2PP(int pid, int processNbr, int messageNbr, vector<process_s> processes, function<void(msg_s*)> callback) :
+    UDP(pid, processNbr, messageNbr, processes, bind(&P2PP::pp2pReceive, this, placeholders::_1)), triggerCallbackP2PP(callback) {};
+
 };
 
--- da18_project/udp.hpp
+++ da18_project/udp.hpp
@@ -1,8 +1,15 @@
+// Main
+#include <stdio.h>
+#include <unistd.h>
+#include <cstring>
+// Socket
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <arpa/inet.h>
+// Structs
 #include <vector>
 #include <string>
+#include <functional>
 
 using namespace std;
 
@@ -30,15 +37,17 @@ private :
   function<void(msg_s*)> triggerCallbackUDP;
 
 public :
-  UDP::UDP(int pid, int processNbr, int messageNbr, vector <process_s> processes, function<void(msg_s*)> callback) :
-    pid(pid), processNbr(processNbr), messageNbr(message(Nbr)), processes(processes), triggerCallbackUDP(callback);
+  UDP(int pid, int processNbr, int messageNbr, vector <process_s> processes, function<void(msg_s*)> callback) :
+    pid(pid), processNbr(processNbr), messageNbr(messageNbr), processes(processes), triggerCallbackUDP(callback) {
+
+  };
 
   void send(msg_s msg, int dst, int sockfd) {
     sockaddr_in dest_addr;
-    bzero(&dest_addr,sizeof(dest_addr));
+    memset(&dest_addr, 0, sizeof(dest_addr));
     dest_addr.sin_family = AF_INET;
-    dest_addr.sin_addr.s_addr = inet_addr(ips[dst].c_str());
-    dest_addr.sin_port = htons(ports[dst]);
+    dest_addr.sin_addr.s_addr = inet_addr(processes[dst].ip.c_str());
+    dest_addr.sin_port = htons(processes[dst].port);
     if (sendto(sockfd, (void* ) &msg, sizeof(msg), 0, (const sockaddr*) &dest_addr, sizeof(dest_addr)) == -1) {
       perror("cannot send message");
       exit(1);
@@ -46,8 +55,9 @@ public :
   }
 
   void startReceiving(void) {
-    msg_s* msg;
+    msg_s msg;
     sockaddr_in src_addr;
+    sockaddr_in self_addr;
     socklen_t addrlen = sizeof(src_addr);
 
     int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -56,21 +66,22 @@ public :
       exit(1);
     }
 
-    bzero(&self_addr,sizeof(self_addr));
+    memset(&self_addr, 0, sizeof(self_addr));
     self_addr.sin_family = AF_INET;
-    self_addr.sin_addr.s_addr = inet_addr(ips[pid].c_str());
-    self_addr.sin_port = htons(ports[pid]);
+    self_addr.sin_addr.s_addr = inet_addr(processes[pid].ip.c_str());
+    self_addr.sin_port = htons(processes[pid].port);
     if(bind(sockfd,(struct sockaddr *) &self_addr, sizeof(self_addr)) == -1) {
       perror("cannot bind socket");
       exit(1);
     }
     while(1) {
-      if (recvfrom(sockfd, (void *) msg, sizeof(msg_s), 0, (sockaddr*) &src_addr, (socklen_t *) &addrlen) == -1) {
+      if (recvfrom(sockfd, (void *) &msg, sizeof(msg_s), 0, (sockaddr*) &src_addr, (socklen_t *) &addrlen) == -1) {
         perror("cannot receive message");
         exit(1);
-      }
-      triggerCallbackUDP(msg)
+      };
+      printf("Received message\n");
+      triggerCallbackUDP(&msg);
     }
   }
-}
+};
 
