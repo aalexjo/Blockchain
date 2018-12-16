128de0f000bdfc37749e521d181173a3f7009a10
Rafael Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2018-12-16 15:16:00 +0100

    Added dependencies

--- da18_project/test_correctness.sh
+++ da18_project/test_correctness.sh
@@ -18,13 +18,17 @@ sudo tc qdisc change dev lo root netem delay 50ms 200ms loss 10% 25% reorder 25%
 
 # compile (should output: da_proc)
 make
-
 echo "5
 1 127.0.0.1 11001
 2 127.0.0.1 11002
 3 127.0.0.1 11003
 4 127.0.0.1 11004
-5 127.0.0.1 11005" > membership
+5 127.0.0.1 11005
+1 2 3 4 5
+2 1 3 4 5
+3 2 1 4 5
+4 2 3 1 5
+5 2 3 4 1" > membership
 
 # start 5 processes, each broadcasting 100 messages
 for i in `seq 1 5`
